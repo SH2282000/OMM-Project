8bd63ce5e163da924fd65d9976dd84fcc2c6752d
Shannah Santucci <shannah.santucci@gmail.com>
2023-11-30 23:34:19 +0100

    First draft.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,43 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+    <body>
+      <header>
+        <div className="topbar">
+          <div className="logo_group">
+            <img src="mini_logo.svg" alt="Mini Logo" />
+          </div>
+          <menu>
+            <li><a href="/">Login</a></li>
+            <li><a href="/">News</a></li>
+            <li><a href="/">Contact</a></li>
+            <li><a href="/">About</a></li>
+          </menu>
+          <textarea rows={1} className="searchbox" placeholder="search..." />
+        </div>
+        <hr className="solid" />
+      </header>
+      <div id="app">
+        <div>
+          <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
+            <img src="lmu.jpg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
+            <img src="vite.svg" className="logo vanilla" alt="TypeScript logo" />
+          </a>
+          <h1>Hello</h1>
+          <div className="card">
+            <button id="counter" type="button"></button>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and TypeScript logos to learn more
+          </p>
+        </div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </body>
     </>
   )
 }
