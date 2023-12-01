b6de659e4d6ebdb6b393754028439dd12c4dd6f5
Shannah Santucci <shannah.santucci@gmail.com>
2023-12-01 01:26:53 +0100

    Add login page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,30 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './LoginPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <body>
-      <header>
-        <div className="topbar">
-          <div className="logo_group">
-            <img src="mini_logo.svg" alt="Mini Logo" />
+      <body>
+        <header>
+          <div className="topbar">
+            <div className="logo_group">
+              <img src="mini_logo.svg" alt="Mini Logo" />
+            </div>
+            <menu>
+              <li><a href="/">Login</a></li>
+              <li><a href="/">News</a></li>
+              <li><a href="/">Contact</a></li>
+              <li><a href="/">About</a></li>
+            </menu>
+            <textarea rows={1} className="searchbox" placeholder="search..." />
           </div>
-          <menu>
-            <li><a href="/">Login</a></li>
-            <li><a href="/">News</a></li>
-            <li><a href="/">Contact</a></li>
-            <li><a href="/">About</a></li>
-          </menu>
-          <textarea rows={1} className="searchbox" placeholder="search..." />
-        </div>
-        <hr className="solid" />
-      </header>
-      <div id="app">
-        <div>
-          <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-            <img src="lmu.jpg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
-            <img src="vite.svg" className="logo vanilla" alt="TypeScript logo" />
-          </a>
-          <h1>Hello</h1>
-          <div className="card">
-            <button id="counter" type="button"></button>
-          </div>
-          <p className="read-the-docs">
-            Click on the Vite and TypeScript logos to learn more
-          </p>
-        </div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </body>
+          <hr className="solid" />
+        </header>
+        <Login />
+      </body>
     </>
   )
 }
